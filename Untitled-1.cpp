3da1ea3b8d3086e88c8449fc9e49aeaeb08f9e40
Yinyin <1468547965@qq.com>
2022-04-07 19:20:54 +0800

    保存

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
-int main()
-{
-    using namespace std;
-    cout << "Come up and C++ me some time.\n";
-    cout << endl;
-    cout << "You won't regret it!" << endl;
+#include <string>
+using namespace std;
+int main(){
+    for (int i =0;i<2;i++){
+        string city,state;
+        getline(cin,city,',');
+        getline(cin,state);
+        cout<<"City:"<<city<<"State:"<<state<<endl;
+    }
     return 0;
 }
